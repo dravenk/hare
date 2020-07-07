f3a1df7727237b9b5cffdb4f2908c839f9f64eb0
dravenk <longxianwen@outlook.com>
2020-07-08 03:17:09 +0800

    update

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Header(){
+    return (
+        <header className="header">
+            <p>
+             This is header!
+            </p>
+        </header>
+    )
+}
+
---
+++ src/components/MainContent.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function MainContent(){
+    return (
+        <div className="main-content">
+          Edit <pre> src/App.js</pre> and savess ffsdsto reload.
+     </div>
+    )
+}
+
